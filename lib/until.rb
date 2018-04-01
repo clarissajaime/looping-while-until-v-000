24ef63905f104d52c5e8cd500a763c4b3c3bef4e
Clarissa Jaime <clarissanjaime@gmail.com>
2018-04-01 22:24:59 +0000

    automatically backed up by learn

--- lib/until.rb
+++ lib/until.rb
@@ -1,6 +1,10 @@
 def using_until
-        levitation_force = 6
-        #your code here
-    
-end
+    levitation_force = 6
+    #your code here
 
+    until levitation_force = 11
+      levitation_force += 1
+      # puts "You have now said the command #{levitation_force} times"
+    end
+
+end
